be62db1787478803170210015eff2141ff6ae41e
Макс Титов <hhit934@gmail.com>
2025-05-04 20:06:58 +0300

    Windows (/SUBSYSTEM:WINDOWS)

--- Chrome.WidgetAutoHide/Chrome.WidgetAutoHide.cpp
+++ Chrome.WidgetAutoHide/Chrome.WidgetAutoHide.cpp
@@ -87,12 +87,12 @@ void main_l() {
 	CloseHandle(threadloop);
 }
 
-int main()
-{
-	main_l();
-}
-//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+//int main()
 //{
 //	main_l();
-//	return 0;
 //}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	main_l();
+	return 0;
+}
